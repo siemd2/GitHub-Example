d94dd55d81ea53fde32cca49b807f0b9b2786090
Siem Debesay <siemd2@109d25lx1sgf8j7.local>
2019-02-12 13:10:42 -0700

    Initial Commit

---
+++ GitHub Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Example
+//
+//  Created by Siem Debesay on 2019-02-12.
+//  Copyright © 2019 Siem Debesay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
